3f796c144546cd1858a3b99949a47e2dcd503077
vannn-tech <jovan20166@gmail.com>
2026-05-04 18:42:19 +0700

    Add Definisi Constructor dan Pembuatan Object

--- constructor.cpp
+++ constructor.cpp
@@ -5,3 +5,12 @@ class mahasiswa {
 public:
     mahasiswa(); // Constructor
 };
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
+};
+
+int main() {
+    mahasiswa mhs; // Pembuatan Object
+    return 0;
+}
