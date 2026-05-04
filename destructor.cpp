4515fede88d0744d946ea8e36b8f8bbe4ad500ac
vannn-tech <jovan20166@gmail.com>
2026-05-04 18:51:36 +0700

    Add main function with stack and heap object creation

--- destructor.cpp
+++ destructor.cpp
@@ -35,3 +35,21 @@ void angka::cetakData()
     }
 }
 
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);                    // Constructor Dipanggil
+    angka *ptrBelajarcpp = new angka(5);   // Constructor Dipanggil
+    delete ptrBelajarcpp;                  // Destructor Dipanggil
+
+    return 0;
+}
