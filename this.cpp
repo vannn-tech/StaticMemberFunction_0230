0dbc1f04f5677e34d728a62406a9506e48ff80cd
vannn-tech <jovan20166@gmail.com>
2026-05-04 18:53:50 +0700

    Add setget method using this pointer

--- this.cpp
+++ this.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 class buku{
     string judul;
-    
+    public:
+    string setget(string judul){
+        this->judul = judul; //nilai parameter 'judul' untuk member variabel 'judul'
+        return this->judul; // return variabel judul.
+    }
 };
 
